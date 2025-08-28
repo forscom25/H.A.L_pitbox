09dd6bc4415be60984c0965a61f1bf452a9a20d6
AhnSunJae <epigon123@naver.com>
2025-08-28 09:00:19 +0900

    Update formula_autonomous_system_node.hpp

--- formula_autonomous_system_node.hpp
+++ formula_autonomous_system_node.hpp
@@ -137,7 +137,9 @@ public: // Function components
     void publishCenterLineMarker();
     void publishGlobalConesMarker();
     void publishLaneMarker();
-    
+    void publishStartFinishLineMarker();
+    void publishGlobalPathMarker();                  // Globalpath
+    void publishTrajectoryFromGlobalPathMarker();    // TrajectoryFromGlobalpath
 
 // Variables
 private:
@@ -192,6 +194,8 @@ public: // ROS
     ros::Publisher lap_count_marker_pub_;
     ros::Publisher global_cones_marker_pub_;
     ros::Publisher lane_marker_pub_;
+    ros::Publisher global_path_marker_pub_;                    // Globalpath
+    ros::Publisher trajectory_from_global_path_marker_pub_;    // TrajectoryFromGlobalpath
 
     // Output messages
     fs_msgs::ControlCommand control_command_msg_;
